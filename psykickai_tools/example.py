eb5fb7025ab46f53eeb6fc6896a051fa81d02186
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-01-28 11:59:56 +0530

    add: renamed and modified the main tools directory

---
+++ psykickai_tools/example.py
@@ -0,0 +1,7 @@
+from psykickai_tools.utils import logger
+
+# Usage examples:
+logger.debug("Debug message")
+logger.info("Info message")
+logger.warning("Warning message")
+logger.error("Error message")
