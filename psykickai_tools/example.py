a46d52feec900805d4496cbf55efad9e320b42bb
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-01-28 13:07:59 +0530

    remove: redundant files

--- psykickai_tools/example.py
+++
@@ -1,7 +0,0 @@
-from psykickai_tools.utils import logger
-
-# Usage examples:
-logger.debug("Debug message")
-logger.info("Info message")
-logger.warning("Warning message")
-logger.error("Error message")
